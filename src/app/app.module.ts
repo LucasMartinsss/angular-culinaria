49fa96187bfece706a56623863a662cd49512506
LucasMartinsss <lucasemartinsd@gmail.com>
2018-11-07 00:35:07 -0200

    Criação de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./home/header/header.component";
+import { MainComponent } from './home/main/main.component';
+import { NewRecipeComponent } from './home/new-recipe/new-recipe.component';
+import { ViewRecipesComponent } from './home/view-recipes/view-recipes.component';
+import { DetailRecipeComponent } from './home/detail-recipe/detail-recipe.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, MainComponent, NewRecipeComponent, ViewRecipesComponent, DetailRecipeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
